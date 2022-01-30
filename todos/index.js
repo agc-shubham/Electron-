b9d8a27196313b5bbbfc74423bc035829b90420a
agcShubham <agc19shubham@gmail.com>
2022-01-30 19:57:29 +0530

    Finished Todo App

--- todos/index.js
+++ todos/index.js
@@ -1,9 +1,10 @@
 const electron = require('electron');
 // const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+let addWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         webPreferences: {
@@ -11,22 +12,84 @@ app.on('ready', () => {
             contextIsolation: false,
         }
     });
-    mainWindow.loadURL(`file://${__dirname}/main.html`)
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed', () => { app.quit() });
 
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
+
+    // const ses = mainWindow.webContents.session
+    // console.log(ses.getUserAgent())
+});
+
+function createaddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: "Add New Todo",
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`)
+    addWindow.on('closed', () => addWindow = null);
+}
+
+
+
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:add', todo);
+    addWindow.close();
 });
 
 const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            {label: 'New Todo'},
-            {label: 'Quit'}
+            {
+                label: 'New Todo',
+                click() { createaddWindow(); }
+            },
+            {
+                label: 'Delete Todo',
+                click() { mainWindow.webContents.send('todo:delete');}
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform === "win32" ? 'Ctrl+Q' : 'Control+Q',
+                // (() => {
+                //     if (process.platform === 'darwin') {
+                //         return "Command+Q";
+                //     } else if (process.platform === 'win32') {
+                //         return "Ctrl+Q";
+                //     }
+                // })()
+
+                click() {
+                    app.quit();
+                }
+            }
         ]
     }
 ];
 
-if(process.platform === 'darwin'){
+if (process.platform === 'darwin') {
     menuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'View',
+        submenu: [
+            {role: 'reload'},
+            {
+            label: 'Toggle Developer Tools',
+            accelerator: process.platform === 'win32' ? 'Ctrl+Shift+I' : 'Command+Alt+I',
+            click(item, focusedWindow) {
+                focusedWindow.toggleDevTools();
+            }
+        }]
+    })
+
 }
